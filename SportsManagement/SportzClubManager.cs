f9c4726948744d97c9f4613471c8239d3a1960b6
tanish-si <140160497+tanish-si@users.noreply.github.com>
2023-08-10 12:41:40 +0530

    Update SportzClubManager.cs

--- SportsManagement/SportzClubManager.cs
+++ SportsManagement/SportzClubManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,10 +10,22 @@ namespace SportsManagement
     {
         private List<Athlete> athletes = new List<Athlete>();
         private List<Event> events = new List<Event>();
+        private List<Performance> performances = new List<Performance>();
+        private int nextEventId = 1;
 
         public int RegisterAthlete(string firstName, string lastName, int age, string sport, DateTime registrationDate)
         {
-            int athleteId = athletes.Count > 0 ? athletes.Max(a => a.AthleteId) + 1 : 1;
+            int athleteId;
+
+            if (athletes.Count > 0)
+            {
+                athleteId = athletes.Max(a => a.AthleteId) + 1;
+            }
+            else
+            {
+                athleteId = 1;
+            }
+
             athletes.Add(new Athlete
             {
                 AthleteId = athleteId,
@@ -28,7 +40,16 @@ namespace SportsManagement
 
         public string ViewAthleteDetails(int athleteId)
         {
-            var athlete = athletes.FirstOrDefault(a => a.AthleteId == athleteId);
+            Athlete athlete = null;
+            foreach (var a in athletes)
+            {
+                if (a.AthleteId == athleteId)
+                {
+                    athlete = a;
+                    break;
+                }
+            }
+
             if (athlete != null)
             {
                 return $"Name: {athlete.FirstName} {athlete.LastName}\nAge: {athlete.Age}\nSport: {athlete.Sport}\nRegistration Date: {athlete.RegistrationDate}";
@@ -38,7 +59,16 @@ namespace SportsManagement
 
         public string UpdateAthleteDetails(int athleteId, string firstName, string lastName, int age, string sport)
         {
-            var athlete = athletes.FirstOrDefault(a => a.AthleteId == athleteId);
+            Athlete athlete = null;
+            foreach (var a in athletes)
+            {
+                if (a.AthleteId == athleteId)
+                {
+                    athlete = a;
+                    break; // Exit the loop once a match is found
+                }
+            }
+
             if (athlete != null)
             {
                 athlete.FirstName = firstName;
@@ -52,7 +82,15 @@ namespace SportsManagement
 
         public string RemoveAthlete(int athleteId)
         {
-            var athlete = athletes.FirstOrDefault(a => a.AthleteId == athleteId);
+            Athlete athlete = null;
+            foreach (var a in athletes)
+            {
+                if (a.AthleteId == athleteId)
+                {
+                    athlete = a;
+                    break; // Exit the loop once a match is found
+                }
+            }
             if (athlete != null)
             {
                 athletes.Remove(athlete);
@@ -60,5 +98,150 @@ namespace SportsManagement
             }
             return "Athlete not found.";
         }
+
+        public int CreateEvent(string eventName, string eventType, DateTime eventDate)
+        {
+            int eventId = nextEventId; // Use the next available ID
+
+            events.Add(new Event
+            {
+                EventId = eventId,
+                EventName = eventName,
+                EventType = eventType,
+                EventDate = eventDate
+            });
+
+            nextEventId++; // Increment for the next event
+            return eventId;
+        }
+
+        public string ViewEventDetails(int eventId)
+        {
+            Event eventData = null;
+            foreach (var e in events)
+            {
+                if (e.EventId == eventId)
+                {
+                    eventData = e;
+                    break;
+                }
+            }
+
+            if (eventData != null)
+            {
+                return $"Event Name: {eventData.EventName}\nEvent Type: {eventData.EventType}\nEvent Date: {eventData.EventDate}";
+            }
+            return "Event not found.";
+        }
+
+        public string UpdateEventDetails(int eventId, string eventName, string eventType, DateTime eventDate)
+        {
+            Event eventData = null;
+            foreach (var a in events)
+            {
+                if (a.EventId == eventId)
+                {
+                    eventData = a;
+                    break; // Exit the loop once a match is found
+                }
+            }
+
+            if (eventData != null)
+            {
+                eventData.EventName = eventName;
+                eventData.EventType = eventType;
+                eventData.EventDate = eventDate;
+
+                return "Update successful.";
+            }
+            return "Event not found.";
+        }
+        public string RemoveEvent(int eventId)
+        {
+            Event eventData = null;
+            foreach (var a in events)
+            {
+                if (a.EventId == eventId)
+                {
+                    eventData = a;
+                    break; // Exit the loop once a match is found
+                }
+            }
+            if (eventData != null)
+            {
+                events.Remove(eventData);
+                return "Event removed successfully.";
+            }
+            return "Event not found.";
+        }
+
+        public void AthletePerformance(int eventId, int athleteId, string score, DateTime currentTime)
+        {
+            // Validate if eventId and athleteId are valid before proceeding
+            bool validEvent = events.Any(e => e.EventId == eventId);
+            bool validAthlete = athletes.Any(a => a.AthleteId == athleteId);
+
+            if (!validEvent)
+            {
+                Console.WriteLine("Invalid EventId.");
+                return;
+            }
+
+            if (!validAthlete)
+            {
+                Console.WriteLine("Invalid AthleteId.");
+                return;
+            }
+
+            // Create a new Performance object and add it to the performances list
+            performances.Add(new Performance
+            {
+                AthleteId = athleteId, // Assign the athlete ID
+                Score = score,
+                CurrentTime = currentTime
+            });
+        }
+
+        public string ViewAthletePerformance(int athleteId)
+        {
+            string performanceDetails = "";
+            Athlete athlete = null;
+            foreach (var a in athletes)
+            {
+                if (a.AthleteId == athleteId)
+                {
+                    athlete = a;
+                    break;
+                }
+            }
+
+            if (athlete != null)
+            {
+                performanceDetails += $"Athlete: {athlete.FirstName} {athlete.LastName}\n";
+                performanceDetails += "Performance:\n";
+
+                var athletePerformances = performances.Where(p => p.AthleteId == athleteId);
+
+                foreach (var performance in athletePerformances)
+                {
+                    performanceDetails += $"Score: {performance.Score}\n";
+                    performanceDetails += $"Time: {performance.CurrentTime}\n";
+                   
+                }
+
+                return performanceDetails;
+            }
+            return "Athlete not found.";
+
+
+
+
+
+        }
     }
 }
+
+
+
+    
+
