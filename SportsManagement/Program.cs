1bb068decd9c78b08c2d7652325b959c13ac5e44
tanish-si <140160497+tanish-si@users.noreply.github.com>
2023-08-10 12:42:16 +0530

    Update Program.cs

--- SportsManagement/Program.cs
+++ SportsManagement/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace SportsManagement
+namespace SportsManagement
 {
     internal class Program
     {
@@ -6,17 +6,32 @@
         {
             SportzClubManager clubManager = new SportzClubManager();
 
-            // Register Athletes
+           
             int athlete1Id = clubManager.RegisterAthlete("Tanish", "Parmar", 22, "Cricket", DateTime.Now);
-            int athlete2Id = clubManager.RegisterAthlete("Harsh", "Sanas", 21, "Basketball", DateTime.Now);
+            int athlete2Id = clubManager.RegisterAthlete("Harsh", "Sanas", 21, "Football", DateTime.Now);
 
-            // View Athlete Details
             Console.WriteLine("Athlete Details:");
             Console.WriteLine(clubManager.ViewAthleteDetails(athlete1Id));
+            Console.WriteLine();
             Console.WriteLine(clubManager.ViewAthleteDetails(athlete2Id));
             Console.WriteLine();
 
+            int event1Id = clubManager.CreateEvent("Cricket", "Tournament", DateTime.Now);
+            int event2Id = clubManager.CreateEvent("Football", "Tournament", DateTime.Now);
 
+            Console.WriteLine("Event Details:");
+            Console.WriteLine(clubManager.ViewEventDetails(event1Id));
+            Console.WriteLine();
+            Console.WriteLine(clubManager.ViewEventDetails(event2Id));
+            Console.WriteLine();
+
+            clubManager.AthletePerformance(athlete1Id, event1Id, "101*(89) ", DateTime.Now);
+            clubManager.AthletePerformance(athlete2Id, event2Id, "5-4 ", DateTime.Now);
+
+
+            Console.WriteLine("View Performance:");
+            Console.WriteLine(clubManager.ViewAthletePerformance(athlete1Id));
+            Console.WriteLine(clubManager.ViewAthletePerformance(athlete2Id));
         }
     }
 }
