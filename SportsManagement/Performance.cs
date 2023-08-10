7134ab39d8e32125ec4b2e2228a20c1a34866011
tanish-si <140160497+tanish-si@users.noreply.github.com>
2023-08-10 12:42:47 +0530

    Add files via upload

---
+++ SportsManagement/Performance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsManagement
+{
+    internal class Performance
+    {
+        public int AthleteId { get; set; }
+
+        public string Score { get; set; }
+        public DateTime CurrentTime { get; set; }
+    }
+}
